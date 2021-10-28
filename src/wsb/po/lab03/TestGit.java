e8987a39e3b827eecfc3ae5dfa51e4842a9a15a7
Piotr Bobiński <piotr.bobinski@wsb.warszawa.pl>
2021-10-28 12:26:01 +0200

    drugi commit

--- src/wsb/po/lab03/TestGit.java
+++ src/wsb/po/lab03/TestGit.java
@@ -3,5 +3,7 @@ package wsb.po.lab03;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Cześć GIT");
+
+        System.out.println("zmiana");
     }
 }
